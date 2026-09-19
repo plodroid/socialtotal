418d092d6a00d0b327357acbb797fec1c03cced6
plodroid <138373351+plodroid@users.noreply.github.com>
2026-09-19 11:38:36 +0100

    Add safe public demo configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+window.SOCIALTOTAL_CONFIG = {
+  googleClientId: "",
+  tiktokClientKey: "",
+  backendBaseUrl: "",
+  discordClientId: "",
+  adsenseClient: "",
+  adsenseSlot: "",
+  demoMode: true
+};
